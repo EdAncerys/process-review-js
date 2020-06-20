5547baa1cb2989261301603fe528992503d2cd97
Ed <lookatemail@gmail.com>
2020-06-20 13:42:16 +0100

    File structure setup

---
+++ jasmine-standalone-3.5.0/spec/LeapYearsSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Leap Years', function () {
+  var years;
+
+  beforeEach(function () {
+    years = new LeapYears();
+  });
+
+  it('should be able to return a if a passed in', () => {
+    // expect(years.returnLeapYears('a')).toEqual('a');
+  });
+});
