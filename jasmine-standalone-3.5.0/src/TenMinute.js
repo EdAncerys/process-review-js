03796eb26ac5e6135243ed3e7c05a7f4618d3afb
Ed <lookatemail@gmail.com>
2020-06-20 14:50:50 +0100

    Refactor class TenMinuteWalk code

--- jasmine-standalone-3.5.0/src/TenMinute.js
+++ jasmine-standalone-3.5.0/src/TenMinute.js
@@ -1,8 +1,29 @@
 'use strict';
 
 class TenMinuteWalk {
+  constructor() {
+    this.n = 0;
+    this.e = 0;
+    this.s = 0;
+    this.w = 0;
+  }
   returnTenMinute(walk) {
-    if (walk.length == 2) {
+    walk.forEach((element) => {
+      if (element == 'n') {
+        this.n += 1;
+      }
+      if (element == 'e') {
+        this.e += 1;
+      }
+      if (element == 's') {
+        this.s += 1;
+      }
+      if (element == 'w') {
+        this.w += 1;
+      }
+    });
+
+    if (this.w == this.e) {
       return true;
     } else {
       return false;
