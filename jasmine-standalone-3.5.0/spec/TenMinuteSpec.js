4dd9a121ee071f00839217e3efcdf5c5bc92d926
Ed <lookatemail@gmail.com>
2020-06-20 14:15:58 +0100

    Project file structure setup

---
+++ jasmine-standalone-3.5.0/spec/TenMinuteSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Leap Years', function () {
+  var walk;
+
+  beforeEach(function () {
+    walk = new TenMinuteWalk();
+  });
+
+  it('should be able to return true if 2000 passed in', () => {
+    // expect(years.returnMinuteWalk(2000)).toEqual(true);
+  });
+});
