b09813620e0fe4674c387d9272932526e5003bdc
Ed <lookatemail@gmail.com>
2020-06-22 09:26:21 +0100

    Test pass: able to return 100 if AA passed in

--- jasmine-standalone-3.5.0/spec/CheckoutSpec.js
+++ jasmine-standalone-3.5.0/spec/CheckoutSpec.js
@@ -6,19 +6,27 @@ describe('Checkout', () => {
     checkout = new Checkout();
   });
 
-  it('should be able to return 50 if A passed in', () => {
-    expect(checkout.totalPurchase('A')).toEqual(50);
+  describe('Cart with one item in', () => {
+    it('should be able to return 50 if A passed in', () => {
+      expect(checkout.totalPurchase('A')).toEqual(50);
+    });
+
+    it('should be able to return 30 if B passed in', () => {
+      expect(checkout.totalPurchase('B')).toEqual(30);
+    });
+
+    it('should be able to return 20 if C passed in', () => {
+      expect(checkout.totalPurchase('C')).toEqual(20);
+    });
+
+    it('should be able to return 15 if D passed in', () => {
+      expect(checkout.totalPurchase('D')).toEqual(15);
+    });
   });
 
-  it('should be able to return 30 if B passed in', () => {
-    expect(checkout.totalPurchase('B')).toEqual(30);
-  });
-
-  it('should be able to return 20 if C passed in', () => {
-    expect(checkout.totalPurchase('C')).toEqual(20);
-  });
-
-  it('should be able to return 15 if D passed in', () => {
-    expect(checkout.totalPurchase('D')).toEqual(15);
+  describe('Cart with multiple items in', () => {
+    it('should be able to return 100 if AA passed in', () => {
+      expect(checkout.totalPurchase('AA')).toEqual(100);
+    });
   });
 });
